9bd3312a47728759774d287fef06cf03b689b5ef
Mariana <mariana@cypcumbres.mx>
2026-08-13 11:00:59 -0600

    lib: agrega forward_auth_headers compartido

--- libs/cumbresbi-scope/cumbresbi_scope/__init__.py
+++ libs/cumbresbi-scope/cumbresbi_scope/__init__.py
@@ -1,6 +1,7 @@
 from .scope import EffectiveScope
 from .managers import ScopedManager, ScopedQuerySet, ScopedModelMixin
 from .middleware import EffectiveScopeMiddleware
+from .forwarding import forward_auth_headers
 
 __all__ = [
     "EffectiveScope",
@@ -8,4 +9,5 @@ __all__ = [
     "ScopedQuerySet",
     "ScopedModelMixin",
     "EffectiveScopeMiddleware",
+    "forward_auth_headers",
 ]
